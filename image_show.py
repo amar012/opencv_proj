e68b397875ce55425d64713225d0a43c09f8b850
amar2012 <amarenderreddy012@gmail.com>
2018-07-31 15:12:40 +0530

    refactored image_show.py using 'nargs' parameter of argparse 'add_argument()', to take a list of image file paths to display

--- image_show.py
+++ image_show.py
@@ -1,13 +1,26 @@
 import cv2
 import argparse
+import os
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True,
-	help="path to image file")
+ap.add_argument("-i", "--images", nargs='+', required=True,
+	help="image file path(s)\nseparate consecutive paths with a space")
 
-vars = ap.parse_args()
+args = ap.parse_args()
 
-img = cv2.imread(vars.image)
-cv2.imshow("Image", img)
-cv2.waitKey(0)
+def im_show(img_path_list):
+
+    for img_path in args.images:
+        if not os.path.exists(img_path) or not os.path.isfile(img_path):
+            print("The image path: '%s' is not a valid path/file" % (img_path))
+        else:
+            img = cv2.imread(img_path)
+            cv2.imshow(img_path, img)
+            cv2.waitKey(0)
+
+
 cv2.destroyAllWindows()
+
+if __name__ == '__main__' :
+
+    im_show(args.images)
